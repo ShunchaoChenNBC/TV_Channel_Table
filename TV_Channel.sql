afa6b2c047d6a526567219f217e31fb3871073d9
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-03-14 18:44:24 -0400

    Update TV_Channel.sql
    Change the timestamp to start_time

--- TV_Channel.sql
+++ TV_Channel.sql
@@ -3,7 +3,7 @@ create or replace table `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.TV_Channels` as
 
 select 
 a.adobe_date,
-a.Hour,
+EXTRACT(hour from a.Start_Time) as Start_Hour,
 a.adobe_tracking_id,
 lower(a.Channels) as Channels,
 round(sum(a.num_seconds_played_no_ads)/3600,2) as Watch_Hours, -- 0.01 intervel
@@ -11,18 +11,17 @@ round(sum(a.num_seconds_played_no_ads)/60,2) as Watch_Minutes -- add minute colu
 from
 (SELECT 
 adobe_date,
-EXTRACT(HOUR FROM adobe_timestamp) as Hour,
+timestamp_sub(adobe_timestamp, interval cast(num_seconds_played_no_ads as INT64) second) as Start_Time,
 adobe_tracking_id,
 case when consumption_type = 'Virtual Channel' then display_name
 	         else playlist_name end as Channels,
 num_seconds_played_no_ads 
 FROM `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` 
-WHERE adobe_date between "2022-11-01" and "2023-03-08"
+WHERE adobe_date between "2022-11-01" and "2023-03-13"
 and num_seconds_played_no_ads > 0) a
 where 1=1
 and (lower(Channels) LIKE "%-tv%" or Channels like "% | %" or length(Channels) = 4 or (regexp_contains(Channels, r"\w{4}-\w{2}") and length(Channels) <= 7)) 
 and lower(Channels) not in ("kane","edge","omos","otis","cnbc","news","imsa","bige","golf")
 group by 1,2,3,4
-
 
 
