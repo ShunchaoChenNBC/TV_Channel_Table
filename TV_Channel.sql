7e91eefd0b1de59a6db536cd75fc8dae57d8665f
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-04-03 12:46:44 -0400

    Update TV_Channel.sql
    Extend the table to include device information

--- TV_Channel.sql
+++ TV_Channel.sql
@@ -6,6 +6,8 @@ a.adobe_date,
 EXTRACT(hour from a.Start_Time) as Start_Hour,
 a.adobe_tracking_id,
 lower(a.Channels) as Channels,
+device_platform,
+device_name,
 round(sum(a.num_seconds_played_no_ads)/3600,2) as Watch_Hours, -- 0.01 intervel
 round(sum(a.num_seconds_played_no_ads)/60,2) as Watch_Minutes -- add minute column for 5 minute cutoff
 from
@@ -15,13 +17,14 @@ timestamp_sub(adobe_timestamp, interval cast(num_seconds_played_no_ads as INT64)
 adobe_tracking_id,
 case when consumption_type = 'Virtual Channel' then display_name
 	         else playlist_name end as Channels,
+device_platform,
+device_name,
 num_seconds_played_no_ads 
 FROM `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` 
-WHERE adobe_date between "2022-11-01" and "2023-03-13"
+WHERE adobe_date between "2022-11-01" and "2023-04-02"
 and num_seconds_played_no_ads > 0) a
 where 1=1
 and (lower(Channels) LIKE "%-tv%" or Channels like "% | %" or length(Channels) = 4 or (regexp_contains(Channels, r"\w{4}-\w{2}") and length(Channels) <= 7)) 
 and lower(Channels) not in ("kane","edge","omos","otis","cnbc","news","imsa","bige","golf")
-group by 1,2,3,4
-
+group by 1,2,3,4,5,6
 
